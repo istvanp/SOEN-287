b5bd8c60146ec00f9dd303d3e74a6d841035dff1
Istvan Pusztai <istvanp@gmail.com>
2013-02-05 05:21:39 -0500

    Update javascript/slides/29.js

--- javascript/slides/29.js
+++ javascript/slides/29.js
@@ -1,5 +1,5 @@
 /* @title Exercise
-   @desc See instructions in the comments.<br><a href="https://github.com/istvanp/tutorials/blob/gh-pages/javascript/slides/29-exercise-solution.html">Solution</a>
+   @desc See instructions in the comments.<br><a target="_blank" href="https://github.com/istvanp/tutorials/blob/gh-pages/javascript/slides/29-exercise-solution.html">Solution</a>
 
 Create an XHTML file with embeded JavaScript that does the following:
  1. Asks the user for how many data points (integers or floats) he/she wishes to enter.
