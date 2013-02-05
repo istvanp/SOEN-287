2dd2b5e90a81a3cd59663ae2e631da972364f3d2
Istvan Pusztai <istvanp@gmail.com>
2013-02-05 02:09:39 -0500

    Cleaned up editor code

--- javascript/editor.js
+++ javascript/editor.js
@@ -1,25 +1,96 @@
 jQuery(function() {
     var slide = null, save = false,
-        $autorun = $('#autorun')
+        $autorun = $('#autorun'),
         editor = ace.edit("editor"),
         titleRE = /@title ?(.*)\n/,
         descRE  = /@desc ?(.*)\n/;
+
+    // Set code editor options
     editor.setTheme("ace/theme/tomorrow_night");
     editor.getSession().setMode("ace/mode/javascript");
     editor.getSession().setUseWrapMode(true);
 
-    if (save && sessionStorage['code']) {
-        editor.setValue(sessionStorage['code']);
-        editor.gotoLine(1);
-    }
+    // Slide changed event
+    $(window).hashchange(function() {
+        var hash = location.hash;
+        var slideResult = hash.match(/#(\d+)/);
 
-    if (sessionStorage['autorun'] == 1) {
-        $autorun.prop('checked', true);
-    } else {
-        $autorun.prop('checked', false);
-    }
+        if ( ! slideResult) {
+            window.location.hash = '0';
+            return;
+        }
 
-    var update = function(e) {
+        slide = parseInt(slideResult[1], 10);
+
+        // Jump to slide 0
+        if (isNaN(slide) || slide < 0) {
+            window.location.hash = '0';
+            return;
+        }
+
+        if (slide === 0) {
+            editor.setValue('');
+            editor.gotoLine(1);
+            $('#prev').prop('disabled', true);
+            $('#next').prop('disabled', false);
+            clear();
+            return;
+        }
+
+        if (slide > 0) {
+            $('#prev').prop('disabled', false);
+        }
+
+        if (save && sessionStorage[slide])
+        {
+            editor.setValue(sessionStorage[slide]);
+            editor.gotoLine(1);
+            return;
+        }
+
+        $.ajax({
+            url: 'slides/' + slide + '.js',
+            success: function(code) {
+                clear();
+                sessionStorage[slide] = code;
+                editor.setValue(code);
+                editor.gotoLine(1);
+                $('#next').prop('disabled', false);
+            },
+            error: function(e) {
+                window.location.hash = --slide;
+                $('#next').prop('disabled', true);
+            },
+            dataType: 'text'
+        });
+    });
+
+    var runCode = function() {
+        var code = editor.getValue();
+        code = code.replace(/document\.write/g, 'print');
+        code = code.replace(/document\.writeln/g, 'println');
+        clear();
+        try {
+            (new Function(code)).call();
+        } catch(e) {
+            error(e);
+        }
+    };
+
+    var clear = function() {
+        $('#output ol').empty();
+    };
+
+    var next = function() {
+        window.location.hash = ++slide;
+    };
+
+    var prev = function() {
+        window.location.hash = --slide;
+    };
+
+    // Code modified event
+    editor.getSession().on('change', function(e) {
         var code    = editor.getValue();
             title   = code.match(titleRE),
             desc    = code.match(descRE),
@@ -42,91 +113,37 @@ jQuery(function() {
             sessionStorage[slide] = code;
         }
 
-        if ($autorun.prop('checked')) {
-            $('#run').trigger('click');
-        }
-    };
-    editor.getSession().on('change', update);
-
-    $('#run').on('click', function() {
-        var code = editor.getValue();
-        code = code.replace(/document\.write/g, 'print');
-        code = code.replace(/document\.writeln/g, 'println');
-        $('#clear').trigger('click');
-        try {
-            (new Function(code)).call();
-        } catch(e) {
-            error(e);
-        }
+        $autorun.trigger('change');
     });
 
-    $('#clear').on('click', function() {
-        $('#output ol').empty();
-    });
-
-    $('#next').on('click', function() {
-        window.location.hash = ++slide;
-    });
-
-    $('#prev').on('click', function() {
-        window.location.hash = --slide;
-    });
-
+    // Auto-run state changed
     $autorun.on('change', function() {
-        console.log($(this).prop('checked'));
-        sessionStorage['autorun'] = 0 + $(this).prop('checked');
+        var checked = $(this).prop('checked');
+        sessionStorage['autorun'] = 0 + checked;
+
+        if (checked) {
+            runCode();
+        }
     });
 
-    $(window).hashchange(function() {
-        var hash = location.hash;
-        var slideResult = hash.match(/#(\d+)/);
-        if ( ! slideResult) {
-            window.location.hash = '0';
-            return;
-        }
-        slide = slideResult[1];
-        if (slide < 0) {
-            window.location.hash = '0';
-            return;
-        }
-        if (slide == 0) {
-            editor.setValue('');
-            editor.gotoLine(1);
-            $('#prev').prop('disabled', true);
-            $('#next').prop('disabled', false);
-            $('#clear').trigger('click');
-            return;
-        }
-        if (slide > 0) {
-            $('#prev').prop('disabled', false);
-        }
+    // Attach Events
+    $('#run').on('click', runCode);
+    $('#clear').on('click', clear);
+    $('#next').on('click', next);
+    $('#prev').on('click', prev);
 
-        if (save && sessionStorage[slide])
-        {
-            editor.setValue(sessionStorage[slide]);
-            editor.gotoLine(1);
-            return;
-        }
+    // Init auto-run status
+    if (sessionStorage['autorun'] == 1) {
+        $autorun.prop('checked', true);
+    } else {
+        $autorun.prop('checked', false);
+    }
 
-        $.ajax({
-            url: 'slides/' + slide + '.js',
-            success: function(code) {
-                sessionStorage[slide] = code;
-                editor.setValue(code);
-                editor.gotoLine(1);
-                $('#next').prop('disabled', false);
-                $('#clear').trigger('click');
-            },
-            error: function(e) {
-                window.location.hash = --slide;
-                $('#next').prop('disabled', true);
-            },
-            dataType: 'text'
-        });
-    });
+    // Init the slide
     $(window).hashchange();
 });
 
+// Define replacement for document.write()
 function print(str) {
     if (arguments.length == 0) {
         str = '';
@@ -139,6 +156,7 @@ function print(str) {
     }
 }
 
+// Define replacement for document.writeln()
 function println(str) {
     if (arguments.length == 0) {
         str = '';
@@ -146,6 +164,7 @@ function println(str) {
     $('#output ol').append('<li>' + str + '</li>');
 }
 
+// Output an error
 function error(str) {
     $('#output ol').append('<li style="color: red">' + str + '</li>');
 }
