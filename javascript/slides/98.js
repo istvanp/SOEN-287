3c70617a0409854420e4e12f692f1f9e075f4e70
Istvan Pusztai <istvanp@gmail.com>
2013-02-12 10:44:39 -0500

    Update javascript/slides/98.js
    Fixed typo

--- javascript/slides/98.js
+++ javascript/slides/98.js
@@ -1,5 +1,5 @@
 // @title Regular Expressions: match anything but a white space character
-// @desc Use <code>\S</code> to match any white space character. Equivalent to <code>[ \f\n\r\t\v​\u00A0\u1680​\u180e\u2000​\u2001\u2002​\u2003\u2004​\u2005\u2006​\u2007\u2008​\u2009\u200a​\u2028\u2029​\u2028\u2029​\u202f\u205f​\u3000]</code>.
+// @desc Use <code>\S</code> to match any white space character. Equivalent to <code>[^ \f\n\r\t\v​\u00A0\u1680​\u180e\u2000​\u2001\u2002​\u2003\u2004​\u2005\u2006​\u2007\u2008​\u2009\u200a​\u2028\u2029​\u2028\u2029​\u202f\u205f​\u3000]</code>.
 
 var re = /\S+/;
 
