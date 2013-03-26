95e1cea224fd9af9100a263305e3fa2a2ecfc81a
Istvan Pusztai <istvanp@gmail.com>
2013-03-26 04:45:10 -0400

    Added XML slides

---
+++ xml/js/slides.js
@@ -0,0 +1,199 @@
+jQuery(function() {
+    var slide    = null, data = null, doEval = true, doTest = false,
+        isLocal = /^http:\/\/localhost/.test(window.location.href),
+        previousCode = '', inProgress = false,
+        $content = $('#slide-outer'),
+        $title   = $('#slide-inner h1'),
+        $desc    = $('#slide-inner div'),
+        $editor  = $('#editor');
+        $output  = $('#output');
+
+
+    // Set code editor options
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/tomorrow");
+    editor.getSession().setMode("ace/mode/php");
+    editor.getSession().setUseWrapMode(true);
+    editor.getSession().setTabSize(2);
+    editor.getSession().on('change', function(e) {
+        var height = editor.getSession().getScreenLength();
+        $editor.height(20 * height + 'px');
+        editor.resize();
+    });
+
+    $.ajax({
+        url: 'data.yml',
+        dataType: 'text',
+        success: function(_data) {
+            data = jsyaml.load(_data);
+            $(window).trigger('hashchange');
+        }
+    });
+
+    var evaluate = function(testOutput) {
+        if (doEval === false || inProgress) {
+            return;
+        }
+
+        var code = editor.getValue();
+
+        if ( ! code || code.trim().length === 0 || previousCode === code) {
+            return;
+        }
+
+        inProgress = true;
+        $("#ajax" ).show();
+
+        $.ajax({
+            url: (isLocal) ? 'validate.php' : 'http://users.encs.concordia.ca/~i_puszta/validate.php',
+            data: { code: code },
+            dataType: 'jsonp',
+            jsonpCallback: 'callback',
+            success: function(_data) {
+                if (_data === "") {
+                    _data = '<h3 style="text-align:center"><em>No output</em></h3>';
+                }
+
+                $output.html(_data);
+                previousCode = code;
+                
+                if (testOutput &&
+                    testOutput.toString().rtrim() != _data.toString().rtrim()) {
+                    alert('Must update output in YAML');
+                    console.log(testOutput.rtrim());
+                    console.log(_data.rtrim());
+                }
+            },
+            error: function(xhr, status, err) {
+                if (status != 'parsererror') {
+                    return;
+                }
+                previousCode = code;
+                $output.html(xhr.responseText);
+            },
+            complete: function() {
+                inProgress = false;
+                $("#ajax" ).hide();
+            }
+        });
+    };
+
+    $('#next').on('click', function() {
+        window.location.hash = ++slide;
+    });
+    $('#prev').on('click', function() {
+        window.location.hash = --slide;
+    });
+
+    $(window).hashchange(function() {
+        var hash = location.hash;
+        var slideResult = hash.match(/#(\d+)/);
+
+        if ( ! slideResult) {
+            window.location.hash = '1';
+            return;
+        }
+
+        slide = parseInt(slideResult[1], 10);
+
+
+        // Jump to first slide
+        if (isNaN(slide) || slide < 1) {
+            window.location.hash = '1';
+            return;
+        }
+
+        // Jump to last slide
+        if (slide > data.length) {
+            window.location.hash = data.length;
+            return;
+        }
+
+        if (slide === 1) {
+            $('#prev').prop('disabled', true);
+            $('#next').prop('disabled', false);
+        }
+
+        if (slide > 1) {
+            $('#prev').prop('disabled', false);
+        }
+
+        if (data[slide - 1]) {
+            var title = data[slide - 1].title,
+                desc = data[slide - 1].desc,
+                xml = data[slide - 1].xml,
+                pre = data[slide - 1].pre,
+                output = data[slide - 1].output || '<em>No output available for this slide</em>',
+                src,
+                re = /`([^`]*)`/g,
+                replacement = '<code class="prettyprint">$1</code>';
+
+
+            title = title.replace(/&/g,'&amp;')
+                         .replace(/</g,'&lt;')
+                         .replace(/>/g,'&gt;')
+                         .replace(re, replacement);
+            title = slide + ". " + (title || 'No title');
+            if (desc) {
+                desc = desc.replace(/& /g,'&amp; ')
+                           .replace(/\n\n/g, "<br>")
+                           .replace(/```([^`]*)```/g, function(match, p1) {
+                               return '<pre class="prettyprint">' +
+                                      p1.replace(/<br>/g, "\n\n")
+                                        .replace(/</g, '&lt;')
+                                        .replace(/>/g, '&gt;') +
+                                      '</pre>';
+                           })
+                           .replace(re, function(match, p1) {
+                               return '<code class="prettyprint">' +
+                                      p1.replace(/</g, '&lt;')
+                                        .replace(/>/g, '&gt;') +
+                                      '</code>';
+                           });
+            }
+            
+            $title.html(title);
+            $desc.html(desc || '');
+
+            if ( ! xml) {
+                $editor.hide();
+                $output.hide();
+            }
+            else
+            {
+                editor.setValue(xml);
+                editor.gotoLine(1);
+                $editor.show();
+                $output.show();
+            }
+            
+            if (pre === true) {
+                $output.addClass('pre');                
+            } else {
+                $output.removeClass('pre');
+            }
+
+            if (doTest) {
+                evaluate(data[slide - 1].output);
+            } else {
+                evaluate();
+            }
+
+            $output.html(output);
+            prettyPrint();
+        }
+        if ( ! data[slide])
+        {
+            $('#next').prop('disabled', true);
+        }
+        else {
+            $('#next').prop('disabled', false);
+        }
+    });
+
+    window.setInterval(evaluate, 500);
+});
+
+String.prototype.rtrim = function(){
+    return this.replace(/\s+$/gm,'');
+};
---
+++ xml/validate.php
@@ -0,0 +1,83 @@
+<?php
+ini_set('html_errors', 1);
+ob_start();
+
+// Enable advanced libxml errors
+libxml_use_internal_errors(true);
+
+$xmlStr = stripslashes($_GET['code']);
+$xml = explode("\n", $xmlStr);
+$xml[-1] = "No source available for this line.";
+$dom = new DOMDocument();
+$dom->strictErrorChecking = false; // Allow malformed XML
+
+// Don't try to validate if XML is malformed
+if ($dom->loadXML($xmlStr))
+{
+    $dom->validate();
+}
+
+$errors = libxml_get_errors();
+libxml_clear_errors();
+
+if (empty($errors)) {
+    echo '<div class="success">XML is well formed and valid!</div>';
+}
+else if (current($errors)->code == 522)
+{
+    echo '<div class="success">XML is well formed!<br>',
+         '<small>No validation was performed because no DTD was found.</small></div>';
+    $errors = array();
+}
+
+foreach ($errors as $error) {
+    echo display_xml_error($error, $xml);
+}
+
+$out = ob_get_contents();
+ob_end_clean();
+$out = json_encode($out);
+echo "callback($out)";
+
+function display_xml_error($error, $xml)
+{
+    $return = "<div class=\"error\">\n";
+    $return .= "<span class=\"code\">";
+
+    switch ($error->level) {
+        case LIBXML_ERR_WARNING:
+            $return .= "Warning $error->code";
+            break;
+         case LIBXML_ERR_ERROR:
+            $return .= "Error $error->code";
+            break;
+        case LIBXML_ERR_FATAL:
+            $return .= "Fatal error $error->code";
+            break;
+    }
+    
+    $return .= "</span> on ";
+    $return .= "<span class=\"line\">line " . $error->line . "</span> ";
+    $return .= "<span class=\"column\">column " . $error->column . "</span>: ";
+    $return .= "<span class=\"message\">" . trim($error->message) . "</span>\n";
+    $line = $error->line == 0 ? -1 : $error->line - 1;
+    $return .= "<div class=\"source\">" .
+                   highlight($xml[$line], $error->column).
+               "</div>";
+    $return .= "</div>";
+
+    return $return;
+}
+function highlight($source, $column)
+{
+    if ($column == 0)
+    {
+        return htmlspecialchars($source);
+    }
+    $str = substr_replace($source, '<*>', $column - 2, 0);
+    $str = substr_replace($str, '</*>', $column + 2, 0);
+    $str = htmlspecialchars($str);
+    $str = str_replace('&lt;*&gt;', '<span>', $str);
+    $str = str_replace('&lt;/*&gt;', '</span>', $str);
+    return $str;
+}
